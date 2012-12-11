8b5bc05de5825b0baaaac1678163437ae5ce5ff5
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-12-11 22:33:15 +0200

    build options, just winging it...

--- hardware/tiny/cores/tiny/core_build_options.h
+++ hardware/tiny/cores/tiny/core_build_options.h
@@ -133,6 +133,33 @@
 
 #endif
 
+/*=============================================================================
+  Build options for the ATtiny81634 processor
+=============================================================================*/
+
+#if defined( __AVR_ATtiny1634__ )
+
+/*
+  The old standby ... millis on Timer 0.
+*/
+#define TIMER_TO_USE_FOR_MILLIS                   0
+
+/*
+  Tone goes on whichever timer was not used for millis.
+*/
+#if TIMER_TO_USE_FOR_MILLIS == 1
+#define TIMER_TO_USE_FOR_TONE                     0
+#else
+#define TIMER_TO_USE_FOR_TONE                     1
+#endif
+
+#define HAVE_ADC                                  1
+
+#define DEFAULT_TO_TINY_DEBUG_SERIAL              0
+
+#endif
+
+
 
 /*=============================================================================
   There doesn't seem to be many people using a bootloader so we'll assume 
