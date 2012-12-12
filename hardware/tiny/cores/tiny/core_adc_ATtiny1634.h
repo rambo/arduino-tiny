63341416f27830e48a06f88d785579409b4cd5ea
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-12-13 00:30:18 +0200

    stub includes for ADC, not implemented yet

---
+++ hardware/tiny/cores/tiny/core_adc_ATtiny1634.h
@@ -0,0 +1,18 @@
+#ifndef core_adc_ATtiny1634_h
+#define core_adc_ATtiny1634_h
+
+#include <avr/io.h>
+#include <binary.h>
+
+#include "core_build_options.h"
+#include "core_macros.h"
+
+/*=============================================================================
+  Veneer for the ATtiny1634 ADC
+=============================================================================*/
+
+
+#error "Not implemented"
+
+
+#endif
