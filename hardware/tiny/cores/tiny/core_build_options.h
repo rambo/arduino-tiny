727fbc84c62e167a9b3a58bb1b0ad12b48f98d2b
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-12-12 22:46:28 +0200

    moved the __AVR_ATtinyX313__ definitions to separate files

--- hardware/tiny/cores/tiny/core_build_options.h
+++ hardware/tiny/cores/tiny/core_build_options.h
@@ -42,25 +42,7 @@
 #endif
 
 #if defined( __AVR_ATtinyX313__ )
-
-/*
-  The old standby ... millis on Timer 0.
-*/
-#define TIMER_TO_USE_FOR_MILLIS                   0
-
-/*
-  Tone goes on whichever timer was not used for millis.
-*/
-#if TIMER_TO_USE_FOR_MILLIS == 1
-#define TIMER_TO_USE_FOR_TONE                     0
-#else
-#define TIMER_TO_USE_FOR_TONE                     1
-#endif
-
-#define HAVE_ADC                                  0
-
-#define DEFAULT_TO_TINY_DEBUG_SERIAL              0
-
+#include "core_build_options_ATtinyX313.h"
 #endif
 
 
--- hardware/tiny/cores/tiny/pins_arduino.h
+++ hardware/tiny/cores/tiny/pins_arduino.h
@@ -33,9 +33,7 @@
 #include "core_build_options.h"
 
 #if defined( __AVR_ATtinyX313__ )
-#define PORT_A_ID 1
-#define PORT_B_ID 2
-#define PORT_D_ID 4
+#include "pins_arduino_ATtinyX313.h"
 #endif
 
 #if defined( __AVR_ATtinyX4__ )
@@ -96,20 +94,5 @@ extern const uint8_t PROGMEM digital_pin_to_timer_PGM[];
 #define digitalPinToPCMSKbit(p) (((p) <= 2) ? (p) : (10 - (p)))
 #endif
 
-#if defined(__AVR_ATtiny4313__)
-#define digitalPinToPCX(p,s1,s2,s3,s4,s5) \
-    (((p) >= 0) \
-        ? (((p) <=  1) ? (s1)  /*  0 -  1  ==>  D0 - D1 */  \
-        : (((p) <=  3) ? (s2)  /*  2 -  3  ==>  A1 - A0 */  \
-        : (((p) <=  8) ? (s3)  /*  4 -  8  ==>  D2 - D6 */  \
-        : (((p) <= 16) ? (s4)  /*  9 - 16  ==>  B0 - B7 */  \
-        : (s5))))) \
-        : (s5))
-//                                                   s1 D     s2 A     s3 D     s4 B
-#define digitalPinToPCICR(p)    digitalPinToPCX( p, &GIMSK,  &GIMSK,  &GIMSK,  &GIMSK,  NULL )
-#define digitalPinToPCICRbit(p) digitalPinToPCX( p, PCIE2,   PCIE1,   PCIE2,   PCIE0,   0    )
-#define digitalPinToPCMSK(p)    digitalPinToPCX( p, &PCMSK2, &PCMSK1, &PCMSK2, &PCMSK0, NULL )
-#define digitalPinToPCMSKbit(p) digitalPinToPCX( p, p,       3-p,     p-2,     p-9,     0    )
-#endif
 
 #endif
